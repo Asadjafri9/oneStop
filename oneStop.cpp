5571152beae692769e033b4b6f99f21317af219d
Asadjafri9 <asadjaffri79315@gmail.com>
2024-10-22 22:17:52 +0500

    make ticket class

--- oneStop.cpp
+++ oneStop.cpp
@@ -8,9 +8,20 @@ private:
     string name;
     int priority;
     string service;
-    time_t start;
+    int  start_time_min;
     bool status;
-    time_t close;
+    int  end_time_min;
+    public:
+    Ticket(int id , string name, int priority ,string service , int start , bool status , int end ){
+        this -> id = id ;
+        this -> name = name; 
+        this -> priority = priority;
+        this -> service = service ;
+        this -> start_time_min = start ;
+        this -> end_time_min = end;
+        this -> status = status;
+    }
+    
 };
 
 int main()
