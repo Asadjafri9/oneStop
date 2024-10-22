cc99791471bb15f6dc78331e6862bffa1562225d
Asadjafri9 <asadjaffri79315@gmail.com>
2024-10-22 14:37:13 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# oneStop
+ This my Project for one Stop system in FAST NUCES
---
+++ oneStop.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Ticket
+{
+private:
+    int id;
+    string name;
+    int priority;
+    string service;
+    time_t start;
+    bool status;
+    time_t close;
+};
+
+int main()
+{
+    return 0;
+}
